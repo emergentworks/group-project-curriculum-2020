35ff37a18fba6d386c1e9ec9f82fdddfe3fb50cf
Sekou Theanon <sekoutheanon@gmail.com>
2020-02-13 16:38:41 -0500

    Added bootstrap/ local style&script links

--- script.js
+++
@@ -1,7 +0,0 @@
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-}
